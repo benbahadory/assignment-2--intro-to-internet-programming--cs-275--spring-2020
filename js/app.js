0360fb0feb0e346184d2897e21fc91b617dd7b71
benbahadory <60357574+benbahadory@users.noreply.github.com>
2020-04-25 22:16:20 -0400

    Add function to calculate water needed for california rice

--- js/app.js
+++ js/app.js
@@ -1,7 +1,12 @@
 let caliRecipe = document.getElementById(`caliRecipe`);
 let whiteRecipe = document.getElementById(`whiteRecipe`);
+
 let caliButton = document.getElementById(`caliButton`);
 let whiteButton = document.getElementById(`whiteButton`);
+
+let softRiceButton = document.getElementById(`softRice`);
+let ozCaliRice = document.getElementById(`ozCaliRice`);
+let ozCaliWater = document.getElementById(`ozCaliWater`);
 
 let selectCaliRecipe = () => {
     whiteRecipe.style.display = `none`;
@@ -12,5 +17,15 @@ let selectWhiteRecipe = () => {
     whiteRecipe.style.display = `block`;
 };
 
+let calculateCaliWater = () => {
+    if (softRiceButton.checked)
+    {
+        ozCaliWater.value = ozCaliRice.value*2;
+    }
+    else ozCaliWater.value = ozCaliRice.value*1.6;
+};
+
 caliButton.addEventListener(`click`, selectCaliRecipe, false);
 whiteButton.addEventListener(`click`, selectWhiteRecipe, false);
+
+ozCaliRice.addEventListener(`keyup`,calculateCaliWater);
